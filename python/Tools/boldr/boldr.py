6f9f643c86d1bf87ac5a41fa73b639bc1a434f22
Isaiah Magnuson <imagnuson501@gmail.com>
2023-08-28 15:41:41 -0700

    Comment blocks?

--- python/Tools/boldr/boldr.py
+++ python/Tools/boldr/boldr.py
@@ -17,12 +17,13 @@ Changelog:
         allowed text location to be supplied via command line arguments
 """
 
-# IMPORTS:
+# IMPORTS ====================================================================
 import re
-# CONSTANTS:
+# CONSTANTS ==================================================================
 chapterPath = './Hebrews/Hebrews.txt'
 alphaChars = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','-',"'"] # characters that make up legal words
 
+# UTILITIES ==================================================================
 
 
 # goal one: read in a chapter and create a dict of alphabetized words and their occurence numbers
